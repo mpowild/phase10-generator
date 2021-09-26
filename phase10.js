520a5943a900640992fb025a7477cd2ea6401518
mpowild <mark.powild@sainsburys.co.uk>
2021-09-26 18:40:37 +0100

    initial commit

---
+++ phase10.js
@@ -0,0 +1,51 @@
+function getData (){
+
+  var data;
+  $.ajax({
+    type: "GET",
+    url: "phase10_data.csv",
+    dataType: "text",
+    success: function(response)
+    {
+      data = $.csv.toArrays(response);
+      generateHtmlTable(data);
+    }
+  });
+}
+
+function generateHtmlTable(data) {
+  var html = '<table  class="table table-condensed table-hover table-striped">';
+
+  if(typeof(data[0]) === 'undefined') {
+    return null;
+  } else {
+    $.each(data, function( index, row ) {
+      //bind header
+      if(index == 0) {
+        html += '<thead>';
+        html += '<tr>';
+        $.each(row, function( index, colData ) {
+          html += '<th>';
+          html += colData;
+          html += '</th>';
+        });
+        html += '</tr>';
+        html += '</thead>';
+        html += '<tbody>';
+      } else {
+        html += '<tr>';
+        $.each(row, function( index, colData ) {
+          html += '<td>';
+          html += colData;
+          html += '</td>';
+        });
+        html += '</tr>';
+      }
+    });
+    html += '</tbody>';
+    html += '</table>';
+    $('#csv-display').append(html);
+  }
+}
+
+getData();
